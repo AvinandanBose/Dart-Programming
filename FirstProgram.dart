197533d71f353334499941eca352e15b25428011
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2021-09-07 15:18:01 +0530

    Add files via upload

---
+++ FirstProgram.dart
@@ -0,0 +1,8 @@
+
+//This is my first program in dart
+/*This is my first program */
+void main() {
+  print("Hello World");
+  print("This is my first line of code");
+  print(12 / 4);
+}
---
+++ variables.dart
@@ -0,0 +1,20 @@
+void main() {
+  int age = 20;
+  int hex = 0xEADF;
+  var name_1 = "Henry";
+  var age_1 = 21.6; //var can take value and convert to its type
+  double var_1 = 21.12;
+  double exponents = 1.42e5;
+  bool isValid = true;
+  String name = "Henry";
+  print(age);
+  print(var_1);
+  print(name);
+  print(name_1);
+  print(age_1);
+  print(isValid);
+  print(hex);
+  print(exponents);
+}
+// Note every type in dart is a Object
+// Therefore their initial value is null
