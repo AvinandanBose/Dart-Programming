daed572bb6b8bde36031f210d983f8ec257ab07f
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-01-27 00:33:01 +0530

    Add files via upload

--- final and constant.dart
+++ final and constant.dart
@@ -2,18 +2,36 @@ void main() {
   final cityName = 'Mumbai';
   final String countryName = 'India'; //data type is optional
   const PI = 2.14;
-  const double gravity = 9.8;
-  var C = new Circle();
+  const double gravity = 9.8; //data type is optional
+  final C = new Circle();
   print(cityName);
   print(countryName);
   print(PI);
   print(gravity);
   print(C.color);
-  print(PI);
+  C.returns();
+  print(C.printf('red'));
+  declare();
+  
 }
+
+void declare() {
+  const s = 10;
+  const f = 12;
+  print('s=$s');
+  print('f=$f');
+}
+
 
 //final and const in class
 class Circle {
   final color = 'Red';
   static const PI = 3.14; // cannot define constant staright away within
+  void returns() {
+    print(PI);
+  }
+
+  String printf(final colors) {
+    return colors;
+  }
 }
---
+++ func.dart
@@ -0,0 +1,8 @@
+void main() {
+  int ans = sum_two_num(20, 29);
+  print(ans);
+}
+
+int sum_two_num(int start, int end) {
+  return start + end;
+}
